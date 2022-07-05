017eb114e6874bb93474b056518ae77589ca3528
moli <kyeung516@gmail.com>
2022-07-05 19:52:39 +0800

    initialcommit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function pro_img_fun_up() {
+    document.getElementById('Project').src="assets/websiteHtml.png";
+}
+function pro_img_fun_down() {
+    document.getElementById('Project').src="assets/discordBot.png";
+}
